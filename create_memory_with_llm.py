89787b57bbde3b269a170cd64481c12bc4955d84
TARIFUDDIN <rafiuddin.tarif@gmail.com>
2025-03-06 23:34:34 +0530

    lastly commit

--- create_memory_with_llm.py
+++ create_memory_with_llm.py
@@ -1,3 +1,4 @@
+import os  # Add this line
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
